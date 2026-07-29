400d50ac727e10d69171d1346589fd013476d0c1
Dominc Christopher <christopherdominic268@gmail.com>
2026-07-29 17:28:49 -0500

    Refactored many functions to optimize code and create clear separation between calcuation code and streamlit code

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.title("Monte Carlo Estimation of π")
 seed = st.sidebar.number_input("Insert a Seed",min_value=0,max_value=1000000,value=None,step=1)
 rng = np.random.default_rng(seed=seed)
 
-points = [10,100,1000,10000,100000,1000000]
+points = [10,100,1000,10000,100000]
 selected_points = st.sidebar.radio(
     "Select number of points generated",
     points,
@@ -21,49 +21,38 @@ selected_points = st.sidebar.radio(
 )
 
 if st.sidebar.button("Run Simulation"):
-    estimate = u.estimate_pi(selected_points,rng)
-    err_message,err = u.error(estimate)
+    u.estimation(selected_points,rng)
+    # estimate = u.estimate_pi(selected_points,rng)
+    # err_message,err = u.error(estimate)
 
-    estimate_sobol = u.estimate_pi_sobol(selected_points)
-    err_message_sobol,err_sobol = u.error(estimate_sobol)
+    # estimate_sobol = u.estimate_pi_sobol(selected_points)
+    # err_message_sobol,err_sobol = u.error(estimate_sobol)
 
-    st.subheader(f"Estimated π: {round(estimate, 6)}")
-    st.write(f"{err_message}")
+    # st.subheader(f"Estimated π: {round(estimate, 6)}")
+    # st.write(f"{err_message}")
 
-    fig = u.visualize_pi(selected_points,estimate,rng)
-    st.pyplot(fig)
+    # fig = u.visualize_pi(selected_points,estimate,rng)
+    # st.pyplot(fig)
 
-    st.subheader(f"How it works",text_alignment='center')
-    st.write(
-        "The Monte Carlo method uses random sampling to estimate π. We generate random points ina  unit square (0 to 1) on both axes" \
-        "and check whether the poin falls within a quarter of the unit circle(radius=1)." \
-        "The ratio of points within the quarter circle produces an estimate of the quarter circle's area" \
-        "to the square's area. Thus multiplying by 4 gives us π.\n" \
-        "A of Quarter Circle = πr^2/4 when radius = 1 \n" \
-        "A of Unit Square =1" \
-        "=> π = 4 * A of Quarter Circle = points inside/points outside")
+    # st.subheader(f"How it works",text_alignment='center')
+    # st.write(
+    #     "The Monte Carlo method uses random sampling to estimate π. We generate random points ina  unit square (0 to 1) on both axes" \
+    #     "and check whether the poin falls within a quarter of the unit circle(radius=1)." \
+    #     "The ratio of points within the quarter circle produces an estimate of the quarter circle's area" \
+    #     "to the square's area. Thus multiplying by 4 gives us π.\n" \
+    #     "A of Quarter Circle = πr^2/4 when radius = 1 \n" \
+    #     "A of Unit Square =1" \
+    #     "=> π = 4 * A of Quarter Circle = points inside/points outside")
 
-    st.subheader(f"Estimated π using SOBOL Sequence: {round(estimate_sobol, 6)}")
-    st.write(f"{err_message_sobol}")
+    # st.subheader(f"Estimated π using SOBOL Sequence: {round(estimate_sobol, 6)}")
+    # st.write(f"{err_message_sobol}")
     
-    fig_sobol = u.visualize_pi(selected_points,estimate_sobol,rng)
-    st.pyplot(fig_sobol)
-    
-if st.sidebar.button("Plot Convergence"):
+    # fig_sobol = u.visualize_pi(selected_points,estimate_sobol,rng)
+    # st.pyplot(fig_sobol)
 
-    convergence_coords,error_coords,convergence_coords_sobol,error_coords_sobol = u.convergence(points,rng)
-    fig = u.visualize_convergence(convergence_coords,convergence_coords_sobol)
-    st.pyplot(fig)
-
-if st.sidebar.button("Plot Absolute Error"):
-
-    convergence_coords,error_coords,convergence_coords_sobol,error_coords_sobol = u.convergence(points,rng)
-    fig = u.visualize_error(error_coords,error_coords_sobol)
-    st.pyplot(fig)
-
-if st.sidebar.button("Test Plots"):
+if st.sidebar.button("Generate Plots"):
     u.plots(points,rng)
-    
+
 if st.sidebar.button("Run 1000 Repeated Simulations"):
     u.repeated_sims(rng)
 
--- utils.py
+++ utils.py
@@ -27,6 +27,35 @@ def estimate_pi_sobol(samples):
 def error(pi):
     return (f"Error = {abs(round(pi-np.pi,4))}"),abs(round(pi-np.pi,4))
 
+def mean_estimates_and_error(samples,rng):
+
+    estimates = np.empty(500)
+    sobol_estimates = np.empty(500)
+    errors = np.empty(500)
+    sobol_errors = np.empty(500)
+    y_converge = []
+    ys_converge = []
+    y_mae = []
+    ys_mae = []
+    pi = np.pi
+
+    for num in samples:
+        for i in range(500):
+            pi_hat = estimate_pi(num,rng)
+            sobol_pi_hat = estimate_pi_sobol(num)
+
+            estimates[i] = pi_hat
+            sobol_estimates[i] = sobol_pi_hat
+            errors[i] = abs(pi_hat-pi)
+            sobol_errors[i] = abs(sobol_pi_hat-pi)
+
+        y_converge.append(estimates.mean())
+        ys_converge.append(sobol_estimates.mean())
+        y_mae.append(errors.mean())
+        ys_mae.append(sobol_errors.mean())
+
+    return y_converge,ys_converge,y_mae,ys_mae
+
 def visualize_pi(n_points,pi,rng):
     x=rng.random(n_points)
     y=rng.random(n_points)
@@ -49,50 +78,14 @@ def visualize_pi(n_points,pi,rng):
     ax.legend()
     ax.set_title(f"$\\pi$ ({pi}) Estimation with {n_points} points")
     return fig
-
-def convergence(samples,rng):
-    estimates =[]
-    errors=[]
-    estimates_sobol = []
-    errors_sobol=[]
-
-    #Create a list of estimates and errors for each sample size
-    for idx in samples:
-        pi_hat = estimate_pi(idx,rng)
-        estimates.append(pi_hat)
-        err_message,err = error(pi_hat)
-        errors.append(err)
-
-        pi_sobol = estimate_pi_sobol(idx)
-        estimates_sobol.append(pi_sobol)
-        err_sobol_message, err_sobol = error(pi_sobol)
-        errors_sobol.append(err_sobol)
-
-
-    estimate_coords = list(zip(samples,estimates))
-    error_coords = list(zip(samples,errors))
-    estimate_sobol_coords = list(zip(samples,estimates_sobol))
-    error_sobol_coords = list(zip(samples,errors_sobol))
-
-    return estimate_coords, error_coords, estimate_sobol_coords,error_sobol_coords
-
-def visualize_convergence(coords,coords_sobol):
-    #Separate coordinates
-    x = [x for x,y in coords]
-    y = [y for x,y in coords]
-
-    xs=[x for x,y in coords_sobol]
-    ys=[y for x,y in coords_sobol]
-
-    # x,y = zip(*coords)
-    # x = np.array(x,dtype=float)
-    # y = np.array(y, dtype=float)
-
+    
+def visualize_convergence(xcoords,ycoords,yscoords):
+    
     fig, ax = plt.subplots(figsize=(6,6))
     
     #Plot points to show convergence of Pi estimates
-    ax.plot(x,y,color='dodgerblue',label='Mean Estimates')
-    ax.plot(xs,ys,color='indigo',label='Mean Estimates(SOBOL)')
+    ax.plot(xcoords,ycoords,color='dodgerblue',label='Mean Estimates')
+    ax.plot(xcoords,yscoords,color='indigo',label='Mean Estimates(SOBOL)')
     
     #Add boundary for Pi
     ax.axhline(y=np.pi,color='tomato',linestyle='--',linewidth=1.5,label='Pi')
@@ -105,28 +98,24 @@ def visualize_convergence(coords,coords_sobol):
     ax.set_title(f"Convergence Plot of Pi")
     return fig
 
-def visualize_error(coords,coords_sobol):
-    x=[x for x,y in coords]
-    y=[y for x,y in coords]
+def visualize_mae(xcoord,ycoord,yscoord):
 
-    xs=[x for x,y in coords_sobol]
-    ys=[y for x,y in coords_sobol]
-
-    yt = [pow(xt,-0.5) for xt in x]
-    yst =[pow(xt,-1) for xt in x]
+    
+    yt = [pow(xt,-0.5) for xt in xcoord]
+    yst =[pow(xt,-1) for xt in xcoord]
 
     fig, ax = plt.subplots(figsize=(6,6))
 
-    ax.plot(x,y,color="indigo",linestyle='-',linewidth=2,label='Mean Absolute Error')
-    ax.plot(xs,ys,color="orange",linestyle='-',linewidth=2,label='Mean Absolute Error(SOBOL)')
-    ax.plot(x,yt,color="black",linestyle='-.',linewidth=2,label='Theoretical Absolute Error')
-    ax.plot(x,yst,color="green",linestyle='-.',linewidth=2,label='Theoretical Absolute Error(SOBOL)')
+    ax.plot(xcoord,ycoord,color="indigo",linestyle='-',linewidth=2,label='MAE')
+    ax.plot(xcoord,yscoord,color="tomato",linestyle='-',linewidth=2,label="MAE (SOBOL)")
+    ax.plot(xcoord,yt,color="black",linestyle='-.',linewidth=2,label='Theoretical Absolute Error')
+    ax.plot(xcoord,yst,color="green",linestyle='-.',linewidth=2,label='Theoretical Absolute Error(SOBOL)')
 
     ax.margins(x=0.05)
     ax.set_xscale("log")
     ax.set_yscale("log")
     ax.set_ylim(1e-4,1e0)
-    ax.set_xlim(10,1000000)
+    ax.set_xlim(10,100000)
     ax.legend()
     ax.set_title(f"Absolute Error vs Sample Size")
     return fig
@@ -141,12 +130,6 @@ def histogram(estimates,sobol_estimates,bins):
     ax.set_title(f"Comparison of Estimates: N=1000; Number of Samples = 100")
     return fig
 
-def std_dev(estimates, mean,n_points):
-    deviations = [(x-mean)**2 for x in estimates]
-    sum = np.sum(deviations)
-    var = np.sqrt(sum/(n_points-1))
-    return var, round(var,5)
-
 def mse(estimates,mean,n_points):
     error = [(x-mean) for x in estimates]
     sq_error = [x**2 for x in error]
@@ -159,33 +142,35 @@ def mae(errors,n_points):
     maer = sum/n_points
     return round(maer,5)
 
-def descriptive_stats(estimates,errors):
+def descriptive_stats(estimates: np.array,errors: np.array):
     
-    mean = sum(estimates)/len(estimates)
+    mean = estimates.mean()
     msqe= mse(estimates,mean,len(estimates))
     maer = mae(errors,len(errors))
-    var, sd=std_dev(estimates,mean,len(estimates))
-    min_est=min(estimates)
-    max_est=max(estimates)
+    var = (estimates.std())**2
+    sd = estimates.std()
+    min_est=estimates.min()
+    max_est=estimates.max()
 
-    return round(mean,5), msqe,maer, var,sd,min_est, max_est
+    return round(mean,5), msqe,maer, round(var,5),round(sd,5),min_est, max_est
 
 def repeated_sims(rng):
-    estimates = []
-    sobol_estimates = []
-    errors = []
-    sobol_errors = []
+    estimates = np.empty(1000)
+    sobol_estimates = np.empty(1000)
+    errors = np.empty(1000)
+    sobol_errors = np.empty(1000)
+    pi = np.pi
 
     for i in range(1000):
         pi_hat = estimate_pi(1000,rng)
         pi_hat_sobol = estimate_pi_sobol(1000)
-        err_message,err = error(pi_hat)
-        sobol_err_message, sobol_error = error(pi_hat_sobol)
+        err = abs(pi_hat-pi)
+        sobol_error = abs(pi_hat_sobol-pi)
 
-        estimates.append(pi_hat)
-        errors.append(err)
-        sobol_estimates.append(pi_hat_sobol)
-        sobol_errors.append(sobol_error)
+        estimates[i] = pi_hat
+        errors[i] = err
+        sobol_estimates[i] = pi_hat_sobol
+        sobol_errors[i] = sobol_error
 
 
     fig = histogram(estimates,sobol_estimates,10)
@@ -220,44 +205,53 @@ def repeated_sims(rng):
     st.dataframe(table1)
 
 def plots(samples,rng):
-    estimates = []
-    errors = []
-    sobol_estimates = []
-    sobol_errors = []
-    mean_estimates = []
-    mean_errors = []
-    sobol_mean_estimates = []
-    sobol_mean_errors = []
+    
+    
+    y_converge,ys_converge,y_mae,ys_mae = mean_estimates_and_error(samples,rng)
 
-    for num in samples:
-        for i in range(500):
-            pi_hat = estimate_pi(num,rng)
-            err_message,err = error(pi_hat)
-            sobol_pi_hat = estimate_pi_sobol(num)
-            sobol_err_message,sobol_err = error(sobol_pi_hat)
+       
 
-            estimates.append(pi_hat)
-            errors.append(err)
-            sobol_estimates.append(sobol_pi_hat)
-            sobol_errors.append(sobol_err)
+    convergence_fig = visualize_convergence(samples,y_converge,ys_converge)
+    st.pyplot(convergence_fig)
+    error_fig = visualize_mae(samples,y_mae,ys_mae)
+    st.pyplot(error_fig)
 
-        mean,msqe,maer,sd,min_est,max_est = descriptive_stats(estimates,errors)
-        sobol_mean,sobol_msqe,sobol_maer,sobol_sd,sobol_min_est,sobol_max_est = descriptive_stats(sobol_estimates,sobol_errors)
-        mean_estimates.append(mean)
-        mean_errors.append(maer)
-        sobol_mean_estimates.append(sobol_mean)
-        sobol_mean_errors.append(sobol_maer)
+def estimation(selected_points,rng):
+    pi = np.pi
 
-    estimates_coords = list(zip(samples,mean_estimates))
-    error_coords = list(zip(samples,mean_errors))
-    sobol_estimates_coords = list(zip(samples, sobol_mean_estimates))
-    sobol_error_coords = list(zip(samples,sobol_mean_errors))
+    estimate = estimate_pi(selected_points,rng)
+    err = abs(estimate-pi)
 
-    convergence_plot = visualize_convergence(estimates_coords,sobol_estimates_coords)
-    st.pyplot(convergence_plot)
+    estimate_sobol = estimate_pi_sobol(selected_points)
+    err_sobol = abs(estimate_sobol-pi)
 
-    error_plot = visualize_error(error_coords,sobol_error_coords)
-    st.pyplot(error_plot)
+    col1,col2 = st.columns(2)
+
+    with col1:
+        st.metric("π",value=f"{estimate}",delta=f"{np.pi}",border=True)
+        st.metric("Abs Error",value=f"{round(err,5)}",border=True)
+        fig = visualize_pi(selected_points,estimate,rng)
+        st.pyplot(fig)
+
+
+    with col2:
+        st.metric("π (SOBOL)",value=f"{estimate_sobol}",delta=f"{np.pi}",border=True)
+        st.metric("Abs Error (SOBOL)",value=f"{round(err_sobol,5)}",border=True)
+        fig_sobol = visualize_pi(selected_points,estimate,rng)
+        st.pyplot(fig_sobol)
+
+    st.subheader(f"How it works",text_alignment='center')
+    st.write(
+        "The Monte Carlo method uses random sampling to estimate π. We generate random points ina  unit square (0 to 1) on both axes" \
+        "and check whether the poin falls within a quarter of the unit circle(radius=1)." \
+        "The ratio of points within the quarter circle produces an estimate of the quarter circle's area" \
+        "to the square's area. Thus multiplying by 4 gives us π.\n" \
+        "A of Quarter Circle = πr^2/4 when radius = 1 \n" \
+        "A of Unit Square =1" \
+        "=> π = 4 * A of Quarter Circle = points inside/points outside")
+
+    
+       
 
 
 
