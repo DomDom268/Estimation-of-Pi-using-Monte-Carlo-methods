5c706f6e667e078b49cd6f17843c9f75149ba480
Dominc Christopher <christopherdominic268@gmail.com>
2026-08-18 19:16:08 -0500

    Updated test suite to reflect changes in utils.py

--- tests/test_estimate_sobol.py
+++ tests/test_estimate_sobol.py
@@ -5,7 +5,7 @@ Unity test for the estimate_pi_sobol function
 """
 import numpy as np
 from scipy.stats import qmc
-from utils import estimate_pi_sobol
+from utils import generate_sobol_points,estimate_pi_sobol
 
 #Mathematical Tests
 def test_estimate_pi_sobol_range():
@@ -13,7 +13,8 @@ def test_estimate_pi_sobol_range():
     points_to_generate = [5,10,15]
 
     for points in points_to_generate:
-        pi_hat,_=estimate_pi_sobol(points,seed)
+        coords = generate_sobol_points(seed,points)
+        pi_hat=estimate_pi_sobol(coords)
 
         assert 0 <= pi_hat <= 4
 
@@ -21,8 +22,11 @@ def test_estimate_pi_sobol_range():
 def test_estimate_pi_sobol_reproducibility():
     seed = 12345
 
-    pi1,points1 = estimate_pi_sobol(1000,seed)
-    pi2,points2 = estimate_pi_sobol(1000,seed)
+    points1 = generate_sobol_points(seed,1000)
+    points2 = generate_sobol_points(seed,1000)
+
+    pi1 = estimate_pi_sobol(points1)
+    pi2 = estimate_pi_sobol(points2)
 
     assert pi1 == pi2
     np.testing.assert_array_equal(points1,points2)    
@@ -31,7 +35,8 @@ def test_estimate_pi_sobol_diff_seeds():
     seed1 = 12345
     seed2 = 54321
 
-    p1,points1 = estimate_pi_sobol(1000,seed1)
-    p1,points2 = estimate_pi_sobol(1000,seed2)
+    points1 = generate_sobol_points(seed1,1000)
+    points2 = generate_sobol_points(seed2,1000)
+
 
     assert not np.array_equal(points1,points2)
--- tests/test_estimators.py
+++ tests/test_estimators.py
@@ -5,36 +5,43 @@ Unit testing
 
 """
 import numpy as np
-from utils import estimate_pi
+from utils import generate_xy,estimate_pi
 
 #Mathematical Tests
 def test_estimate_pi_range():
-    rng = np.random.default_rng(12345)
+    seed=12345
+
     points_to_generate = [5,10,15]
 
     for points in points_to_generate:
-        pi_hat,_,_=estimate_pi(points,rng)
+        x,y=generate_xy(seed,points)
+        pi_hat=estimate_pi(x,y)
 
         assert 0 <= pi_hat <= 4
 
 #Randomness Tests
 def test_estimate_pi_reproducibility():
-    rng1 = np.random.default_rng(12345)
-    rng2 = np.random.default_rng(12345)
+    seed1 = 12345
+    seed2 = 12345
 
-    pi1,x1,y1 = estimate_pi(10,rng1)
-    pi2,x2,y2 = estimate_pi(10,rng2)
-    
-    assert pi1 == pi2
-    np.testing.assert_array_equal(x1,x2)
-    np.testing.assert_array_equal(y1,y2)    
+    x1,y1= generate_xy(seed1,10)
+    x2,y2=generate_xy(seed2,10)
+
+    pi1 = estimate_pi(x1,y1)
+    pi2 = estimate_pi(x2,y2)
+
+    assert pi1==pi2
+    np.testing.assert_equal(x1,x2)
+    np.testing.assert_equal(y1,y2)
+
 
 def test_estimate_pi_sobol_diff_seeds():
-    rng1 = np.random.default_rng(12345)
-    rng2 = np.random.default_rng(54321)
+    seed1 = 12345
+    seed2 = 54321
 
-    pi1,x1,y1 = estimate_pi(10,rng1)
-    pi2,x2,y2 = estimate_pi(10,rng2)
+    x1,y1= generate_xy(seed1,10)
+    x2,y2=generate_xy(seed2,10)
+    
     
     assert not np.array_equal(x1,x2)
     assert not np.array_equal(y1,y2)
